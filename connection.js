1096469e1f0be0e3fed28c835fc6361914b405b1
Ana Oliveira <121502538+Jenemebous@users.noreply.github.com>
2024-02-21 15:48:55 -0300

    Add files via upload

--- connection.js
+++ connection.js
@@ -1,14 +1,31 @@
-const mysql = require("mysql2")
+const mysql = require("mysql2/promise");
 
-const connection = mysql.createPool({
-    host: 'localhost',
+const connectionPool = mysql.createPool({
+    host: "localhost",
+    user: "root",
+    password: "root",
     port: 3306,
-    user: 'root',
-    database: 'meubdd',
+    database: meubdd,
+});
 
-})
-   
+const executeQuery = async (res, tableName) => {
+    try {
+        const [rows, fields] = await connectionPool.query(`SELECT * FROM ${meubdd}.${tableName}`);
+        res.json(rows);
+    } catch (err) {
+        console.error(`Erro ao executar a consulta para ${tableName}:`, err);
+        res.status(500).send("Erro interno do servidor");
+    }
+};
 
-;
+
+app.get("/autores", async (req, res) => await executeQuery(res, 'autores'));
+app.get("/livros", async (req, res) => await executeQuery(res, 'livros'));
+app.get("/emprestimos", async (req, res) => await executeQuery(res, 'emprestimos'));
+
+app.listen(port, () => {
+    console.log(`Servidor rodando na porta ${port}`);
+});
+
 
 module.exports = connection;
