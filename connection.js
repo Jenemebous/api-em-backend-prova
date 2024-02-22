21ad4dc1cd9cdc8a9afc3ce149beddcd7bd8fca3
Ana Oliveira <121502538+Jenemebous@users.noreply.github.com>
2024-02-22 11:43:29 -0300

    Update connection.js

--- connection.js
+++ connection.js
@@ -1,31 +1,39 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+
 const mysql = require("mysql2/promise");
+
 
 const connectionPool = mysql.createPool({
     host: "localhost",
     user: "root",
     password: "root",
     port: 3306,
-    database: meubdd,
+    database: 'bdbiblioteca',
 });
 
-const executeQuery = async (res, tableName) => {
+const port = 80;
+
+
+
+
+app.delete('/alunos/:id', async (req, res) => {
+    const alunoId = req.params.id;
     try {
-        const [rows, fields] = await connectionPool.query(`SELECT * FROM ${meubdd}.${tableName}`);
-        res.json(rows);
-    } catch (err) {
-        console.error(`Erro ao executar a consulta para ${tableName}:`, err);
-        res.status(500).send("Erro interno do servidor");
+        const connection = await connectionPool.getConnection();
+        await connection.query('DELETE FROM alunos WHERE id = ?', [alunoId]);
+        connection.release();
+        res.status(200).send('Aluno deletado com sucesso');
+    } catch (error) {
+        console.error('Erro ao deletar aluno:', error);
+        res.status(500).send('Erro ao processar a solicitação');
     }
-};
+});
 
-
-app.get("/autores", async (req, res) => await executeQuery(res, 'autores'));
-app.get("/livros", async (req, res) => await executeQuery(res, 'livros'));
-app.get("/emprestimos", async (req, res) => await executeQuery(res, 'emprestimos'));
 
 app.listen(port, () => {
-    console.log(`Servidor rodando na porta ${port}`);
+    console.log(`O servidor está no endereço http://localhost:${port}`)
 });
-
-
-module.exports = connection;
